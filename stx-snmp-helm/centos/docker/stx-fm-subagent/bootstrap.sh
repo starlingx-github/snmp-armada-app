2b370655a7f9a506ea139cfffa6c466d1a82cce4
Gonzalo Gallardo <gonzalo.gallardo@windriver.com>
2020-12-11 17:07:35 -0300

    Support enabling DEBUG log mode for all SNMP containers into the POD
    Enable/Disable the verbose logging to debug the application SNMP including master-agent,subagent and trap-subagent running on containers. Add parameter "-DALL" to turn on debugging output.
    
    Story: 2008132
    Task: 41410
    Signed-off-by: Gonzalo Gallardo <gonzalo.gallardo@windriver.com>
    Change-Id: Id0f68ae1bcfba7086ad20a27748cd272c3477d81

--- stx-snmp-helm/centos/docker/stx-fm-subagent/bootstrap.sh
+++ stx-snmp-helm/centos/docker/stx-fm-subagent/bootstrap.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 set -u
 
+# Option handling in sh
+if [ "$OPTIONDEBUG" != "" ]; then
+    case "$OPTIONDEBUG" in
+        -DALL) echo "-DALL - debug option enable"
+            ;;
+        -*) echo "Unrecognized option $OPTIONDEBUG"
+            OPTIONDEBUG=""
+            ;;
+        *) echo "Bad argument $OPTIONDEBUG"
+            OPTIONDEBUG=""
+            ;;
+    esac
+fi
+
 # Internal params
-RUN_CMD="./snmpSubAgent -f -x tcp:localhost:705"
+RUN_CMD="./snmpSubAgent ${OPTIONDEBUG} -f -x tcp:localhost:705"
 
 # Launch
 $RUN_CMD
-
--- stx-snmp-helm/centos/docker/stx-fm-trap-subagent/bootstrap.sh
+++ stx-snmp-helm/centos/docker/stx-fm-trap-subagent/bootstrap.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 set -u
 
+# Option handling in sh
+if [ "$OPTIONDEBUG" != "" ]; then
+    case "$OPTIONDEBUG" in
+        -DALL) echo "-DALL - debug option enable"
+        ;;
+        -*) echo "Unrecognized option $OPTIONDEBUG"
+            OPTIONDEBUG=""
+        ;;
+        *) echo "Bad argument $OPTIONDEBUG"
+            OPTIONDEBUG=""
+        ;;
+    esac
+fi
+
 # Internal params
-RUN_CMD="./wrsAlarmMIB -f -x tcp:localhost:705"
+RUN_CMD="./wrsAlarmMIB ${OPTIONDEBUG} -f -x tcp:localhost:705"
 
 # Launch
 $RUN_CMD
